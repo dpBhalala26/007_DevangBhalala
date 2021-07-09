6738c969d5fb7643493da36e505d640d4c700d77
Devang Bhalala <bhalaladevang@outlook.com>
2021-07-09 16:30:53 +0530

    download twitter samples

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -2,3 +2,7 @@ import nltk                                 # a Python library for NLP
 from nltk.corpus import twitter_samples     # a sample Twitter dataset from NLTK
 import matplotlib.pyplot as plt             # a library for visualization purpose
 import random                               # a pseudo-random number generator
+
+#downloads sample twitter dataset.
+nltk.download('twitter_samples')
+
