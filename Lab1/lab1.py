84205d0760b2cd4b68b2668e96058006c65bf009
Devang Bhalala <bhalaladevang@outlook.com>
2021-07-09 16:27:28 +0530

    imported twitter samples from nltk corpus and pyplot from matplotlib.

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,1 +1,4 @@
-import nltk, scipy, numpy, pandas, matplotlib
+import nltk                                 # a Python library for NLP
+from nltk.corpus import twitter_samples     # a sample Twitter dataset from NLTK
+import matplotlib.pyplot as plt             # a library for visualization purpose
+import random                               # a pseudo-random number generator
