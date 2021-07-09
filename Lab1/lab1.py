2d36128d85e0b7c670f678ab2ae174133b51a534
Devang Bhalala <bhalaladevang@outlook.com>
2021-07-09 16:47:36 +0530

    created a pie chart with configurations.

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -15,3 +15,19 @@ print('Number of negative tweets: ', len(all_negative_tweets))
 print('\nThe type of all_positive_tweets is: ', type(all_positive_tweets))
 print('The type of a tweet entry is: ', type(all_negative_tweets[0]))
 
+# Declare a figure with a custom size
+fig = plt.figure(figsize=(5, 5))
+# labels for the classes
+labels = 'ML-BSB-Lec', 'ML-HAP-Lec','ML-HAP-Lab'
+
+# Sizes for each slide
+sizes = [40, 35, 25]
+# Declare pie chart, where the slices will be ordered and plotted counter-clockwise:
+plt.pie(sizes, labels=labels, autopct='%.2f%%', shadow=True, startangle=90)
+# autopct enables you to display the percent value using Python string formatting.
+# For example, if autopct='%.2f', then for each pie wedge, the format string is '%.2f' and
+
+# Equal aspect ratio ensures that pie is drawn as a circle.
+plt.axis('equal')
+# Display the chart
+plt.show()
